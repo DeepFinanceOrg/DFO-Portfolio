0740314baaaac087e74851f2e1800a628112fc6d
mtrshirazi21 <mtrshirazi21@gmail.com>
2022-04-09 11:32:36 -0700

    Regularization Risk_extractor.py

--- DFO_Portfolio/Risk_extractor.py
+++ DFO_Portfolio/Risk_extractor.py
@@ -1,31 +1,40 @@
-import  numpy as np 
-import pandas as pd 
+import numpy as np , pandas as pd 
 
+
+#TODO: Complete functions notes
 
 class Covariance_base:
-    
-    def __init__(self,
-                 *,
-                 Risk_extractor_Model='Risk_Parity',
-                 Inside_cluster = False
-                 ) -> None:    
+
+    def __init__(
+        self,
+        *,Risk_extractor_Model='Risk_Parity',
+        Inside_cluster = False
+    )-> None:
+        
         """
+        Parameters
+        ----------
+        * : TYPE
+            DESCRIPTION.
+        Risk_extractor_Model : TYPE, optional
+            DESCRIPTION. The default is 'Risk_Parity'.
+        Inside_cluster : TYPE, optional
+            DESCRIPTION. The default is False.
+
         Returns
         -------
         None
             DESCRIPTION.
-                
-           TODO: Complete the notes  
+
         """
-        
         self.__Risk_extractor = Risk_extractor_Model
         self.__Inside_cluster = Inside_cluster
-    
-    
+
     @staticmethod
-    def Risk_Parity(cov) -> np.array:
-        
+    def Risk_Parity(cov)-> np.array:
         """
+        
+
         Parameters
         ----------
         cov : TYPE
@@ -33,62 +42,63 @@ class Covariance_base:
 
         Returns
         -------
-        Risk_Parity : TYPE
-                    DESCRIPTION.
+        ivp : TYPE
+            DESCRIPTION.
 
-           TODO: Complete the notes
         """
-        
         ivp = 1. / np.diag(cov)
         ivp /= ivp.sum()
-        
         return ivp
-    
-    def Get_Cluster_RParity(self,
-                              cov : pd.DataFrame,
-                              cluster_items : pd.DataFrame.iloc) -> int:
-        """
-        Parameters
-        ----------
-        cov : TYPE
-            DESCRIPTION.
-            ...
-        Returns
-        -------
-        int
-            DESCRIPTION.
-            
-           TODO: Complete the notes            
-        """
-        w_ = self.Risk_Parity(
-            cov.iloc[
-                cluster_items,cluster_items
-                ]
-            ).reshape(-1,1)
-        
-        return np.dot(
-            np.dot(w_.T,cov.iloc[cluster_items,cluster_items]),w_
-            )[0,0]
-        
-    @staticmethod
-    def Optimal_Portfolio(cov,*,mu=None) -> np.array:
-        """
-        Parameters
-        ----------
-        cov : TYPE
-            DESCRIPTION.
-        mu : TYPE
-            DESCRIPTION.
-        Returns
-        -------
-        None.
 
-        TODO : Complete the notes  
+    @staticmethod 
+    def Optimal_Portfolio(cov,*,mu=None)-> np.array:
+        """
+        Parameters
+        ----------
+        cov : TYPE
+            DESCRIPTION.
+        * : TYPE
+            DESCRIPTION.
+        mu : TYPE, optional
+            DESCRIPTION. The default is None.
+
+        Returns
+        -------
+        w : TYPE
+            DESCRIPTION.
+
         """
         if mu is None : mu = np.ones(shape=(cov.shape[0],1))
         w = np.dot(np.linalg.inv(cov),mu)
         w /= np.dot(mu.T,w)
         return w 
+
+    def Get_Cluster_RParity(
+        self,*,
+        cov,
+        cluster_items
+    )-> float:
         
-    
-    
+        """
+        Parameters
+        ----------
+        * : TYPE
+            DESCRIPTION.
+        cov : TYPE
+            DESCRIPTION.
+        cluster_items : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        float
+            DESCRIPTION.
+
+        """
+        w_ = self.Risk_Parity(
+            cov.iloc[cluster_items,cluster_items]
+        ).reshape(-1,1)
+        
+        return np.dot(
+            np.dot(w_.T,cov.iloc[cluster_items,cluster_items]),w_
+        )[0,0]
