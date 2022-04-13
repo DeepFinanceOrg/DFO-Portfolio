c2628320b96f3c0d0d56d2c0322905cf161f509e
Morteza Khosravi <mortezakhosravi74@gmail.com>
2022-04-13 17:41:27 +0430

    some modifications on comments

--- DFO_Portfolio/Cluster_extractor.py
+++ DFO_Portfolio/Cluster_extractor.py
@@ -62,7 +62,14 @@ class Cluster_ext:
                         max_num_clusters=None ,
                         random_state = 1
                         ):
-        
+        """
+        Calculates KMeans optimal K using Gap Statistic 
+        Params:
+            feature_matrix: ndarry of shape (n_samples, n_features)
+            nrefs: number of sample reference datasets to create
+            max_num_clusters: Maximum number of clusters to test for
+        Returns: (gaps, optimalK , clusters)
+        """
         """
         KmeanClusterGap:\n
         Clustering with Kmeans method and finding the optimal number of 
@@ -89,28 +96,20 @@ class Cluster_ext:
         gaps = np.zeros((len(range(1, maxClusters)),))
         resultsdf = pd.DataFrame({'clusterCount':[], 'gap':[]})
         for gap_index, k in enumerate(range(1, maxClusters)):
-            # Holder for reference dispersion results
             refDisps = np.zeros(nrefs)
-            # For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
             for i in range(nrefs):
                 
-                # Create new random reference set
                 randomReference = np.random.random_sample(size=feature_matrix.shape)
-                
-                # Fit to it
                 km = KMeans(k)
                 km.fit(randomReference)
                 
                 refDisp = km.inertia_
                 refDisps[i] = refDisp
-            # Fit cluster to original feature_matrix and create dispersion
             km = KMeans(k)
             km.fit(feature_matrix)
             
             origDisp = km.inertia_
-            # Calculate gap statistic
             gap = np.log(np.mean(refDisps)) - np.log(origDisp)
-            # Assign this loop's gap statistic to gaps
             gaps[gap_index] = gap
             resultsdf = resultsdf.append({'clusterCount':k, 'gap':gap}, ignore_index=True)
             optimal_k = gaps.argmax() + 1
